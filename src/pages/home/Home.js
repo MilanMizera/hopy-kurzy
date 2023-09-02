b4e409eb347f48c8aad6265710e7a7efae58cfa3
Milan Mizera <milan.mizera2323@gmail.com>
2023-09-02 18:26:14 +0200

    testimonials section created by bootstrap

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,15 @@ import mainImg from "../../images/home/mainImg.png"
 import { BsEmojiSmile } from "react-icons/bs";
 import { IoManOutline } from "react-icons/io5";
 import { MdTaskAlt } from "react-icons/md";
+//boostrap mdi testimonials component
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 
 export const Home = () => {
@@ -41,11 +50,96 @@ export const Home = () => {
         <h6 className="benefits-text">úkoly k vypracování</h6>
       </div>
 
-
-
-
     </div>
-    
+
+
+    <div className="testimonials-section">
+
+      <MDBContainer className="py-5" id="testimonials-container">
+        <MDBRow className="d-flex justify-content-center">
+          <MDBCol md="10" xl="8" className="text-center">
+            <h3 className="mb-4" id="testimonials-title">Recenze</h3>
+            <p className="mb-4 pb-2 mb-md-5 pb-md-0" id="testimonials-paragaraf">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+              error amet numquam iure provident voluptate esse quasi, veritatis
+              totam voluptas nostrum quisquam eum porro a pariatur veniam.
+            </p>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="text-center d-flex align-items-stretch" >
+          <MDBCol md="4" className="mb-5 mb-md-0 d-flex align-items-stretch" id="testimonials-wrapper">
+            <MDBCard id="testimonials-card" className="testimonial-card">
+              <div
+                className="card-up"
+                style={{ backgroundColor: "#9d789b" }}
+              ></div>
+              <div className="avatar mx-auto bg-white" id="testimonials-card">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+                  className="rounded-circle img-fluid"
+                  id="testimonials-img"
+                />
+              </div>
+              <MDBCardBody>
+                <h4 className="mb-4" id="testimonials-persone-name">Maria Smantha</h4>
+                <hr />
+                <p className="dark-grey-text mt-4" id="testimonials-persone-recenz">
+                  <MDBIcon id="note-icon" fas icon="quote-left" className="pe-2" />
+                  Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
+                  elit.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="4" className="mb-5 mb-md-0 d-flex align-items-stretch" id="testimonials-wrapper">
+            <MDBCard id="testimonials-card" className="testimonial-card">
+              <div
+                className="card-up"
+                style={{ backgroundColor: "#7a81a8" }}
+              ></div>
+              <div className="avatar mx-auto bg-white" id="testimonials-card">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+                  className="rounded-circle img-fluid"
+                />
+              </div>
+              <MDBCardBody>
+                <h4 className="mb-4" id="testimonials-persone-name">Lisa Cudrow</h4>
+                <hr />
+                <p className="dark-grey-text mt-4" id="testimonials-persone-recenz" >
+                  <MDBIcon id="note-icon" fas icon="quote-left" className="pe-2" />
+                  Neque cupiditate assumenda in maiores repudi mollitia
+                  architecto.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="4" className="mb-5 mb-md-0 d-flex align-items-stretch" id="testimonials-wrapper" >
+            <MDBCard id="testimonials-card" className="testimonial-card">
+              <div
+                className="card-up"
+                style={{ backgroundColor: "#6d5b98" }}
+              ></div>
+              <div className="avatar mx-auto bg-white" id="testimonials-card">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+                  className="rounded-circle img-fluid"
+                />
+              </div>
+              <MDBCardBody>
+                <h4 className="mb-4" id="testimonials-persone-name">John Smith</h4>
+                <hr />
+                <p className="dark-grey-text mt-4" id="testimonials-persone-recenz">
+                  <MDBIcon id="note-icon" fas icon="quote-left" className="pe-2" />
+                  Delectus impedit saepe officiis ab aliquam repellat rem unde
+                  ducimus.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   </div>
 
   )

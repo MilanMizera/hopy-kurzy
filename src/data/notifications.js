e4e2d7d5c0d0c8518888a968ee39339e00b8b320
Milan Mizera <milan.mizera2323@gmail.com>
2023-09-03 23:13:18 +0200

    příprava pro toastify notification a firebase

---
+++ src/data/notifications.js
@@ -0,0 +1,22 @@
+// automatické zprávy (flashmessage)
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+
+
+// registration messages
+
+export const succesRegistrationNotify = () => {
+
+    toast.success('Úspěšně jste se zaregistrovali', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+
+}

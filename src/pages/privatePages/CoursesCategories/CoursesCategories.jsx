e31e9a4887d582740765b2a4b954d003a10d15e6
Milan Mizera <milan.mizera2323@gmail.com>
2023-09-13 17:28:40 +0200

    přidání stylů na stránku seznam kurzů private

--- src/pages/privatePages/CoursesCategories/CoursesCategories.jsx
+++ src/pages/privatePages/CoursesCategories/CoursesCategories.jsx
@@ -9,20 +9,6 @@ import { MdTaskAlt, MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { TbCategory2 } from "react-icons/tb";
 import { MdOutlineAccessTime } from "react-icons/md";
 import { ImFileText2 } from "react-icons/im";
-//all courses boostrap component
-import {
-    MDBCardImage,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardFooter,
-    MDBRow,
-    MDBCard,
-    MDBCardBody,
-    MDBCol,
-    MDBContainer,
-    MDBIcon,
-
-} from 'mdb-react-ui-kit';
 
 const CoursesCategories = () => {
 
@@ -43,11 +29,14 @@ const CoursesCategories = () => {
                         <div class="card h-100">
                             <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                    This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This content is a little bit longer.
-                                </p>
+                                <h5 class="card-title">Název třídy</h5>
+
+                                <div className="private-courses-info-wrapper">
+                                    <p className="private-courses-info-paragraf"> <MdOutlineDriveFileRenameOutline className="private-courses-info-icon"></MdOutlineDriveFileRenameOutline><b className="private-courses-info-bold">jméno lektora:</b> Filip</p>
+                                    <p className="private-courses-info-paragraf"> <TbCategory2 className="private-courses-info-icon"></TbCategory2><b className="private-courses-info-bold">Kategorie:</b> programování/php</p>
+                                    <p className="private-courses-info-paragraf">  <MdOutlineAccessTime className="private-courses-info-icon"></MdOutlineAccessTime><b className="private-courses-info-bold">Délka kurzu:</b> 10 hodin</p>
+                                    <p className="private-courses-info-paragraf"> <ImFileText2 className="private-courses-info-icon"></ImFileText2> <b className="private-courses-info-bold">Úkoly k vypracování:</b> ano</p>
+                                </div>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
